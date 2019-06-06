fa6d05a6102ce5bd908b5e121b0cbb421ffee639
yt19860606 <510456345@qq.com>
2019-06-06 17:34:08 +0800

    提交

--- lib/DeviceList/device_list_Screen.dart
+++ lib/DeviceList/device_list_Screen.dart
@@ -19,42 +19,82 @@ const List<Choice> choises = <Choice>[
 
 final Color selectBackColor = Colors.amber;
 
-
 class DeviceListViewScreen extends StatelessWidget {
   @override
-
-
   TabBar _tabBar = new TabBar(
 
-    /*设置选中背景的颜色*/
-      indicator: new BoxDecoration(
-        /*方法一*/
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 51, 87, 171),
-            Color.fromARGB(255, 212, 22, 62)
-          ],
-        ),
-/*方法二使用图片*/
-//                        image: DecorationImage(
-//                            image:
-//        ExactAssetImage("assets/images/virtual_titleBack.png"),
-//                          fit: BoxFit.cover
-//                      ),
-      ),
+      /*设置选中背景的颜色*/
+//      indicator: new BoxDecoration(
+//        /*方法一*/
+//        gradient: LinearGradient(
+//          colors: [
+//            Color.fromARGB(255, 51, 87, 171),
+//            Color.fromARGB(255, 212, 22, 62)
+//          ],
+//        ),
+///*方法二使用图片*/
+////                        image: DecorationImage(
+////                            image:
+////        ExactAssetImage("assets/images/virtual_titleBack.png"),
+////                          fit: BoxFit.cover
+////                      ),
+//      ),
+
+
+      indicator: new ShapeDecoration(
+      color: Colors.green, // 底色
+  // 统一四边颜色和宽度
+//      shape: Border.all(color: Color(0xFF00FFFF),style: BorderStyle.solid,width: 5)
+// 四个边分别指定颜色和宽度， 当只给bottom时与UnderlineInputBorder一致效果
+//          shape: Border(top: b, bottom: b, right: b, left: b)
+// 底部线
+//          shape: UnderlineInputBorder( borderSide:BorderSide(color: Color(0xFFFFFFFF), style: BorderStyle.solid, width: 2))
+// 矩形边色
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)), side: BorderSide(color: Colors.green, style: BorderStyle.solid, width: 5))
+// 圆形边色
+//        shape: CircleBorder(side: BorderSide(color: Color(0xFFFFFF00), style: BorderStyle.solid, width: 2))
+// 体育场（竖向椭圆）
+//        shape: StadiumBorder(side: BorderSide(width: 2, style: BorderStyle.solid, color: Color(0xFF00FFFF))
+// 角形（八边角）边色
+//          shape: BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)), side: BorderSide(color: Color(0xFFFFFFFF), style: BorderStyle.solid, width: 2))
+  ),
+
+
+
+//      indicatorPadding:EdgeInsets.all( 10.0),
+
+      isScrollable: true,
       indicatorColor: selectBackColor,
-      indicatorSize: TabBarIndicatorSize.tab,
+//      indicatorSize: TabBarIndicatorSize.tab,
       unselectedLabelColor: Colors.black,
+
+
       labelColor: Colors.white,
       tabs: choises.map((Choice choise) {
-        return Tab(
-          text: choise.title,
-        );
+        return
+              Tab(
+                
+                child: Container(
+
+                  color: Colors.grey,
+//  padding: EdgeInsets.only(top: 10,bottom: 10),
+                  width: 150,
+                  height: 36,
+                  child: Row(
+
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text(choise.title,),
+                      Icon(Icons.laptop),
+                    ],
+                  ),
+                ),
+              );
       }).toList());
 
+
+
   Widget build(BuildContext context) {
-
-
     print("_tabBar.preferredSize.height = ${_tabBar.preferredSize.height}");
 
     // TODO: implement build
@@ -67,24 +107,32 @@ class DeviceListViewScreen extends StatelessWidget {
           length: choises.length,
           child: Scaffold(
             appBar: PreferredSize(
-                child: AppBar(
-                  backgroundColor: Colors.white,
-                  bottom:_tabBar,
+                child: Container(
+
+//  color: Colors.red,
+//height: 40,
+////  child:Text("测试"),
+//child: Icon(Icons.business),
+
+                  child: AppBar(
+                    automaticallyImplyLeading: false,
+                    backgroundColor: Colors.white,
+                    bottom: _tabBar,
+                  ),
+
+
+
                 ),
-//                preferredSize: Size(double.infinity, 44)),
-//                preferredSize: Size.fromHeight(48)),
-
-
-            preferredSize: Size.fromHeight(_tabBar.preferredSize.height)),
-
+                preferredSize: Size.fromHeight(48)),
             body: TabBarView(
                 children: choises.map((Choice choise) {
               return Padding(
-                padding: EdgeInsets.all(16),
-
-    child: ChoiseDetail(choice: choise,)
+                  padding: EdgeInsets.all(1),
+                  child: ChoiseDetail(
+                    choice: choise,
+                  )
 //                child: buildGridV3(),
-              );
+                  );
             }).toList()),
           )),
     );
@@ -173,6 +221,34 @@ class ChoiseDetail extends StatelessWidget {
             ),
             Text(choice.title),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomAppBar extends StatelessWidget {
+
+  final String title;
+  final double barHeight = 50.0; // change this for different heights
+
+  CustomAppBar(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    final double statusbarHeight = MediaQuery
+        .of(context)
+        .padding
+        .top;
+
+    return new Container(
+      padding: new EdgeInsets.only(top: statusbarHeight),
+      height: statusbarHeight + barHeight,
+      child: new Center(
+        child: new Text(
+          title,
+          style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
         ),
       ),
     );
